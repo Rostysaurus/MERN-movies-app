45f24d0c93939a44029008e08ce8899b0d764418
rostyslav kanibolotskyi <rkanibolotskyi@gmail.com>
2022-07-05 10:29:30 +0200

    dotenv file added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { useEffect, useState } from "react";
 
 
+
 export default function Home() {
   const [movies, setMovies] = useState([])
+  const popularMovies = process.env.REACT_APP_POPULAR
+
+  console.log(popularMovies)
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const res = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=7559e79f7d4b04282d92faaacf49e489&language=en-US&page=1")
+      const res = await fetch(popularMovies)
       const moviesData = await res.json()
       const movies = moviesData.results
 
